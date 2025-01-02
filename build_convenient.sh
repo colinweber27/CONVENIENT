fa6be07166847920b535c9469437bf149fb7f2a3
Colin Weber <colweber@novagpvm04.fnal.gov>
2025-01-02 12:52:56 -0600

    User inputs whether to enable different generators at build stage.

--- build_convenient.sh
+++ build_convenient.sh
@@ -5,6 +5,19 @@ export CONVENIENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 source global_vars.sh
 
+# Prompt the user to decide which generators to set up
+# NuWro?
+read -p "Enable NuWro? (ON/OFF): " nuwro_opt
+# NEUT?
+read -p "Enable NEUT? (ON/OFF). If \"ON\", then also enter GitHub username that is associated with NEUT, and a valid access token." neut_opt
+if [[ $neut_opt == "Y" ]]
+then
+	read -p "Github Username: " github_username
+	read -p "Github access token: " github_NEUT_access_token
+fi
+# GiBUU?
+read -p "Enable GiBUU? (ON/OFF): " gibuu_opt
+
 cd BuildGenerators
 
 source build_generators_gpvm.sh
