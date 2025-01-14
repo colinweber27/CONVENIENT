469a2a01c4c67c510fef3661acc2da66f07395a0
Colin Weber <colweber@novagpvm04.fnal.gov>
2025-01-14 10:24:26 -0600

    Added documentation for build_convenient.sh

--- build_convenient.sh
+++ build_convenient.sh
@@ -1,3 +1,33 @@
+# Author: Colin Weber (webe1077@umn.edu)
+# Date: 14 January 2025
+# Purpose: To build all the generators in Convenient following a user query 
+# as to which generators should be built. Should be run from within the root 
+# CONVENIENT directory. 
+
+# Command: source build_convenient.sh
+
+# Exports:
+#	CONVENIENT_DIR: the root Convenient directory
+
+# Declares:
+#	nuwro_opt: switch for building NuWro. Must be one of {'ON', 'OFF'}.
+#	neut_opt: switch for building NEUT. Must be one of {'ON', 'OFF'}.
+#	gibuu_opt: switch for building GiBUU. Must be one of {'ON', 'OFF'}.
+
+# Optional declares:
+#	Options activated if neut_opt == 'ON'
+#	github_username: 			the GitHub username associated with neut_dev
+#	github_NEUT_access_token: 	a github access token associated with the 
+#								given GitHub username
+
+# Sources:
+#	global_vars.sh: 			various variables primarily labelling 
+#								different CONVENIENT directories relative to 
+#								the root directory.
+#	build_generators_gpvm.sh: 	Shell script for building the generators, 
+#								based on the user-inputted options queried 
+#								in this script
+
 # She-bang!
 #!/bin.bash
 
